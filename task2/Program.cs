ded283d71ee9f9c345d460a346b5d15c460ce966
Ivan <shilkin-ivan@mail.ru>
2022-12-06 21:14:58 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+    Console.WriteLine(num1);
+    }
+    else
+    {
+    Console.WriteLine(num3);
+    }
+}
+else 
+{
+    if (num2 > num3)
+    {
+    Console.WriteLine(num2);
+    }
+    else
+    {
+    Console.WriteLine(num3);
+    }
+}
