a0c1972a495ea295b0c10d089f963272122529c4
Ivan <shilkin-ivan@mail.ru>
2022-12-06 21:25:03 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i % 2==0)
+{
+   Console.WriteLine("Четное");
+}
+else
+{
+   Console.WriteLine("Нечетное");
+}
